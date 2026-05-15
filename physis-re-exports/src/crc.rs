2492b750f99dc931790f6a8408998a2ba8f4acb7
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2026-05-16 01:46:42 +0200

    Trim code that is not currently used

--- physis-re-exports/src/crc.rs
+++ physis-re-exports/src/crc.rs
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use libz_rs_sys::z_off_t;
-use std::ops::{Add, AddAssign, BitXor, BitXorAssign};
-
 /// CRC used for filepath hashes in index file
 pub(crate) struct Jamcrc {
     table: [u32; 256],
@@ -41,98 +38,5 @@ impl Jamcrc {
         }
 
         !(c ^ 0xFFFFFFFF)
-    }
-}
-
-fn crc32(crc: u32, s: &[u8]) -> u32 {
-    unsafe { libz_rs_sys::crc32(crc.into(), s.as_ptr(), s.len() as u32) as u32 }
-}
-
-fn crc32_combine(crc1: u32, crc2: u32, len2: usize) -> u32 {
-    libz_rs_sys::crc32_combine(crc1.into(), crc2.into(), len2 as z_off_t) as u32
-}
-
-/// CRC used for shader keys
-/// Credit to https://github.com/NotNite/crcracker/ for the original Rust code
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default)]
-pub(crate) struct XivCrc32 {
-    pub crc: u32,
-    pub len: usize,
-}
-
-impl XivCrc32 {
-    pub(crate) fn new(crc: u32, len: usize) -> Self {
-        Self { crc, len }
-    }
-}
-
-impl From<&[u8]> for XivCrc32 {
-    fn from(s: &[u8]) -> Self {
-        Self::new(!crc32(0xFFFFFFFF, s), s.len())
-    }
-}
-
-impl<const N: usize> From<&[u8; N]> for XivCrc32 {
-    fn from(s: &[u8; N]) -> Self {
-        Self::new(!crc32(0xFFFFFFFF, s), N)
-    }
-}
-
-impl From<&str> for XivCrc32 {
-    fn from(s: &str) -> Self {
-        Self::from(s.as_bytes())
-    }
-}
-
-impl Add<XivCrc32> for XivCrc32 {
-    type Output = XivCrc32;
-
-    fn add(self, rhs: XivCrc32) -> Self::Output {
-        Self::new(
-            crc32_combine(self.crc, rhs.crc, rhs.len),
-            self.len + rhs.len,
-        )
-    }
-}
-
-impl AddAssign<XivCrc32> for XivCrc32 {
-    fn add_assign(&mut self, rhs: XivCrc32) {
-        self.crc = crc32_combine(self.crc, rhs.crc, rhs.len);
-        self.len += rhs.len;
-    }
-}
-
-impl BitXor<XivCrc32> for XivCrc32 {
-    type Output = XivCrc32;
-
-    fn bitxor(self, rhs: XivCrc32) -> Self::Output {
-        Self::new(self.crc ^ rhs.crc, self.len.max(rhs.len))
-    }
-}
-
-impl BitXorAssign<XivCrc32> for XivCrc32 {
-    fn bitxor_assign(&mut self, rhs: XivCrc32) {
-        self.crc ^= rhs.crc;
-        self.len = self.len.max(rhs.len);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn check_jamcrc() {
-        const CRC: Jamcrc = Jamcrc::new();
-        let bytes: [u8; 9] = [1, 1, 2, 4, 5, 6, 12, 12, 12];
-
-        assert_eq!(2411431516, CRC.checksum(&bytes))
-    }
-
-    #[test]
-    fn check_xivcrc() {
-        let str = "Default";
-
-        assert_eq!(XivCrc32::from(str).crc, 2978997821);
     }
 }
--- physis-re-exports/src/sqpack/mod.rs
+++ physis-re-exports/src/sqpack/mod.rs
@@ -1,19 +1,9 @@
 // SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::io::{Read, Seek, SeekFrom, Write};
-
-use binrw::{BinRead, BinWrite, Endian, binrw};
-use data::{BlockHeader, CompressionMode};
+use binrw::binrw;
 
 use crate::common::Platform;
-use crate::compression::no_header_decompress;
-
-mod data;
-pub use data::SqPackData;
-
-mod db;
-pub use db::SqPackDatabase;
 
 mod index;
 pub use index::{Hash, IndexEntry, IndexType, SqPackIndex};
@@ -55,98 +45,4 @@ pub(crate) struct SqPackHeader {
     #[brw(pad_after = 44)]
     /// The SHA1 of the bytes immediately before this.
     sha1_hash: [u8; 20],
-}
-
-pub(crate) fn read_data_block<T: Read + Seek>(
-    buf: &mut T,
-    endianness: Endian,
-    starting_position: u64,
-) -> Option<Vec<u8>> {
-    buf.seek(SeekFrom::Start(starting_position)).ok()?;
-
-    let block_header = BlockHeader::read_options(buf, endianness, ()).unwrap();
-
-    match block_header.compression {
-        CompressionMode::Compressed {
-            compressed_length,
-            decompressed_length,
-        } => {
-            let mut compressed_data: Vec<u8> = vec![0; compressed_length as usize];
-            buf.read_exact(&mut compressed_data).ok()?;
-
-            let mut decompressed_data: Vec<u8> = vec![0; decompressed_length as usize];
-            if !no_header_decompress(&mut compressed_data, &mut decompressed_data) {
-                return None;
-            }
-
-            Some(decompressed_data)
-        }
-        CompressionMode::Uncompressed { file_size } => {
-            let mut local_data: Vec<u8> = vec![0; file_size as usize];
-            buf.read_exact(&mut local_data).ok()?;
-
-            Some(local_data)
-        }
-    }
-}
-
-/// A fixed version of read_data_block accounting for differing compressed block sizes in ZiPatch files.
-pub(crate) fn read_data_block_patch<T: Read + Seek>(
-    buf: &mut T,
-    endianness: Endian,
-) -> Option<Vec<u8>> {
-    let block_header = BlockHeader::read_options(buf, endianness, ()).unwrap();
-
-    match block_header.compression {
-        CompressionMode::Compressed {
-            compressed_length,
-            decompressed_length,
-        } => {
-            let compressed_length: usize =
-                ((compressed_length as usize + 143) & 0xFFFFFF80) - (block_header.size as usize);
-
-            let mut compressed_data: Vec<u8> = vec![0; compressed_length];
-            buf.read_exact(&mut compressed_data).ok()?;
-
-            let mut decompressed_data: Vec<u8> = vec![0; decompressed_length as usize];
-            if !no_header_decompress(&mut compressed_data, &mut decompressed_data) {
-                return None;
-            }
-
-            Some(decompressed_data)
-        }
-        CompressionMode::Uncompressed { file_size } => {
-            let new_file_size: usize = (file_size as usize + 143) & 0xFFFFFF80;
-
-            let mut local_data: Vec<u8> = vec![0; file_size as usize];
-            buf.read_exact(&mut local_data).ok()?;
-
-            buf.seek(SeekFrom::Current(
-                (new_file_size - block_header.size as usize - file_size as usize) as i64,
-            ))
-            .ok()?;
-
-            Some(local_data)
-        }
-    }
-}
-
-pub(crate) fn write_data_block_patch<T: Write + Seek>(
-    writer: &mut T,
-    endianness: Endian,
-    data: Vec<u8>,
-) {
-    let new_file_size: usize = (data.len() + 143) & 0xFFFFFF80;
-
-    // This only adds uncompressed data for now, to simplify implementation
-    // TODO: write compressed blocks
-    let block_header = BlockHeader {
-        size: (new_file_size - data.len()) as u32, // TODO: i have no idea what this value is from
-        compression: CompressionMode::Uncompressed {
-            file_size: data.len() as i32,
-        },
-    };
-    block_header.write_options(writer, endianness, ()).unwrap();
-
-    data.write(writer).unwrap();
 }
